82984d5cad93bd34970435a854fa52031e585f99
sarahkhaleddd <es.sarah.khaled2023@alexu.edu.eg>
2025-11-22 18:35:46 +0200

    update

---
+++ src/lab_8/InstructorAnalytics.java
@@ -0,0 +1,14 @@
+package lab_8;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sarahkhaled
+ */
+public class InstructorAnalytics {
+    
+}
