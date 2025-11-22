e1d2b975f481a224a2d9c9cd53e3a98cb388c2e1
sarahkhaleddd <es.sarah.khaled2023@alexu.edu.eg>
2025-11-22 19:10:05 +0200

    update

--- src/lab_8/InstructorAnalytics.java
+++ src/lab_8/InstructorAnalytics.java
@@ -1,5 +1,7 @@
 package lab_8;
 
+import java.util.List;
+
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -12,4 +14,52 @@ package lab_8;
  */
 public class InstructorAnalytics {
     
+    
+    public static double getLessonAverageScore(String courseId, String lessonId) {
+        List<Lesson> lessons = JsonDatabaseManager.getLessons(courseId);
+        for (Lesson l : lessons) {
+            if (l.getLessonId().equals(lessonId)) {
+                return l.getAverageScore();
+            }
+        }
+        return 0;
+    }
+    
+    public static double getLessonCompletion(String courseId, String lessonId) {
+        List <Lesson> lessons = JsonDatabaseManager.getLessons(courseId);
+        for (Lesson l : lessons) {
+            if (l.getLessonId().equals(lessonId)) {
+                return l.getCompletionPercentage();
+            }
+        }
+        return 0;
+    }
+    
+    public static double getCourseCompletion(String courseId) {
+        List<Lesson> lessons = JsonDatabaseManager.getLessons(courseId);
+        if (lessons.isEmpty()) return 0;
+
+        double totalCompletion = 0;
+        for (Lesson l : lessons) {
+            totalCompletion += l.getCompletionPercentage();
+        }
+
+        return totalCompletion / lessons.size();
+    }
+    
+    public static double getCourseAverageScore(String courseId) {
+        List<Lesson> lessons = JsonDatabaseManager.getLessons(courseId);
+        if (lessons.isEmpty()) return 0;
+
+        double totalScore = 0;
+        int count = 0;
+
+        for (Lesson l : lessons) {
+            totalScore += l.getAverageScore();
+            count++;
+        }
+
+        return totalScore / count;
+    }
+
 }
